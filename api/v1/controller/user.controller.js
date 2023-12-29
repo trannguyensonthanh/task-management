9dfffbb37bb25c6ae1799f349eb2115467dbf2fc
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-29 16:12:01 +0700

    màn hình nhập email lấy mã OTP

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -1,7 +1,9 @@
 //[POST] /api/v1/users/register
 const md5 = require("md5");
 const User = require("../models/user.model")
-
+const ForgotPassword = require("../models/forgot-password.model")
+const generateHelper = require("../../../helpers/generate")
+const sendMailHelper = require("../../../helpers/sendMail")
 // [post] /api/v1/users/register
 module.exports.register = async (req, res) => {
   req.body.password = md5(req.body.password);
@@ -75,4 +77,47 @@ res.json({
 });
  
 
+}
+
+module.exports.forgotPassword = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+    
+  })
+
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email Không tồn tại!"
+    });
+    return;
+  }
+
+const otp = generateHelper.generateRandomNumber(8)
+const timeExpire = 5;
+
+const objectForgotPassword = {
+  email: email,
+  otp: otp,
+  expireAt: Date.now() + timeExpire*(1000*60)
+}
+
+
+const forgotPassword = new ForgotPassword(objectForgotPassword);
+await forgotPassword.save();
+
+
+
+// gửi email
+const subject = `Mã OTP ĐỂ XÁC MINH LẤY LẠI MẬT KHẨU`
+const html = `Mã OTP ĐỂ XÁC MINH LẤY LẠI MẬT KHẨU LÀ <b>${otp}</b>. Thời hạn sử dụng: ${timeExpire} phút . Lưu ý không được để lộ OTP `;
+sendMailHelper.sendMail(email, subject, html );
+// end gửi email
+
+  res.json({
+    code: 200,
+    message: "Đã gửi mã otp qua email!"
+  });
 }
