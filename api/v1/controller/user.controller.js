4755d36e56f46ab5f4f0af5bda47e33dee4581dd
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-29 17:02:49 +0700

    Trang thông tin cá nhân

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -178,3 +178,21 @@ module.exports.resetPassword = async (req, res) => {
     message: "đổi mật khẩu thành công"
   });
 }
+
+// [Get] /api/v1/password/detail
+module.exports.detail = async (req, res) => {
+  const token = req.cookies.token;
+  console.log(token);
+
+const user = await User.findOne({
+token: token,
+deleted: false
+}).select("-password -token");
+
+  res.json({
+    code: 200,
+    message: "Thanh cong",
+    info: user
+  })
+}
+
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -7,6 +7,7 @@ router.post("/login", controller.login);
 router.post("/password/forgot", controller.forgotPassword);
 router.post("/password/otp", controller.otpPassword);
 router.post("/password/reset", controller.resetPassword);
+router.get("/detail", controller.detail);
 
 
 
