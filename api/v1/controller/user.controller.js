9e1ad73979ee5d1d3f0bf141fa32b0b3d47201da
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-29 15:00:55 +0700

    tính năng đăng nhập

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -2,6 +2,7 @@
 const md5 = require("md5");
 const User = require("../models/user.model")
 
+// [post] /api/v1/users/register
 module.exports.register = async (req, res) => {
   req.body.password = md5(req.body.password);
  
@@ -33,6 +34,45 @@ res.json({
 }); 
 }
 
+
+}
+
+// [post] /api/v1/users/login
+module.exports.login = async (req, res) => {
+const email = req.body.email;
+const password = req.body.password;
+
+const user = await User.findOne({
+  email: req.body.email,
+  deleted: false
+});
+
+if(!user){
+  res.json({
+    code: 400,
+    message: "Email không tồn tại!",
+  }); 
+  return;
+}
+ 
+
+if(md5(req.body.password) !== user.password){
+res.json({
+  code: 400,
+  message: "Sai mật khẩu!"
+});
+return;
+}
+
+
+const token = user.token;
+res.cookie("token", token);
+
+res.json({
+  code: 200,
+  message: "Đăng nhập thành công!",
+  token: token
+});
  
 
 }
