ef83f694d9d2150c89a90813f7f74d4c8d92e34f
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-30 13:26:12 +0700

    chuyển các route thành private

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -182,18 +182,11 @@ module.exports.resetPassword = async (req, res) => {
 
 // [Get] /api/v1/password/detail
 module.exports.detail = async (req, res) => {
-  const token = req.cookies.token;
-  console.log(token);
-
-const user = await User.findOne({
-token: token,
-deleted: false
-}).select("-password -token");
 
   res.json({
     code: 200,
     message: "Thanh cong",
-    info: user
+    info: req.user
   })
 }
 
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controller/user.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.post("/password/forgot", controller.forgotPassword);
 router.post("/password/otp", controller.otpPassword);
 router.post("/password/reset", controller.resetPassword);
-router.get("/detail", controller.detail);
+router.get("/detail", authMiddleware.requireAuth, controller.detail);
 
 
 
